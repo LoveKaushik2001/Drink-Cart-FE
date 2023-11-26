c3ebf458bc0dda9efd361929a5463c180af1e4a5
visak14 <vishalkesharwani1406@gmail.com>
2023-11-26 19:09:19 +0530

    make cardui

--- src/components/DrinkCart.vue
+++ src/components/DrinkCart.vue
@@ -1,17 +1,35 @@
-<script setup lang="ts">
-import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
-</script>
+
 <template>
-  <TabGroup>
-    <TabList>
-      <Tab>To Deliver</Tab>
-      <Tab>Delivered</Tab>
-      <Tab>Not Delivered</Tab>
-    </TabList>
-    <TabPanels>
-      <TabPanel>To Deliver List</TabPanel>
-      <TabPanel>Delivered List</TabPanel>
-      <TabPanel>Not Delivered List</TabPanel>
-    </TabPanels>
-  </TabGroup>
+  
+  <div class="navbar">
+    <div style="left: 0px; top: 5px; position: absolute; flex-direction: column; justify-content: flex-start; align-items: center; gap: 6px; display: inline-flex">
+        <div style="justify-content: flex-start; align-items: flex-start; gap: 56px; display: inline-flex">
+            <div style="color: black; font-size: 16px; font-family: Poppins; font-weight: 600; word-wrap: break-word">To Deliver</div>
+            <div style="color: #FEBA02; font-size: 16px; font-family: Poppins; font-weight: 600; word-wrap: break-word">Delivered</div>
+            <div style="color: black; font-size: 16px; font-family: Poppins; font-weight: 600; word-wrap: break-word">Not Delivered</div>
+        </div>
+        
+        <div style="width: 430px; height: 12px; background: #D9D9D9"></div>
+    </div>
+    </div>
+    
+  
+
 </template>
+
+<style>
+.navbar {
+  position: absolute;
+      top: 2%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 450px;
+       height: 30px;
+       border: 1px solid #ccc;
+       border-radius: 8px;
+       overflow: hidden;
+       box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+       margin: 20px;
+       display: flex;
+}
+</style>
